c76d748cc79a9a9e23094d1aab029e865b1d1aab
dragonjump <116328950+dragonjump@users.noreply.github.com>
2024-11-03 13:20:27 +0800

    update

---
+++ sales-portal-demo/assets/js/main.js
@@ -0,0 +1,65 @@
+(function () {
+  /* ========= Preloader ======== */
+  const preloader = document.querySelectorAll('#preloader')
+
+  window.addEventListener('load', function () {
+    if (preloader.length) {
+      this.document.getElementById('preloader').style.display = 'none'
+    }
+  })
+
+  /* ========= Add Box Shadow in Header on Scroll ======== */
+  window.addEventListener('scroll', function () {
+    const header = document.querySelector('.header')
+    if (!header) {
+      return
+    }
+    if (window.scrollY > 0) {
+      header.style.boxShadow = '0px 0px 30px 0px rgba(200, 208, 216, 0.30)'
+    } else {
+      header.style.boxShadow = 'none'
+    }
+  })
+
+  /* ========= sidebar toggle ======== */
+  const sidebarNavWrapper = document.querySelector(".sidebar-nav-wrapper");
+  const mainWrapper = document.querySelector(".main-wrapper");
+  const menuToggleButton = document.querySelector("#menu-toggle");
+  const loginButton = document.querySelector("#login-btn");
+  const menuToggleButtonIcon = document.querySelector("#menu-toggle i");
+  const overlay = document.querySelector(".overlay");
+
+  loginButton && loginButton.addEventListener("click", () => {
+
+    window.location.href = "home.html"; 
+    setTimeout(() => {
+      window.location.href = "home.html"; 
+    }, 1600);
+  });
+
+  menuToggleButton && menuToggleButton.addEventListener("click", () => {
+    sidebarNavWrapper && sidebarNavWrapper.classList.toggle("active");
+    overlay && overlay.classList.add("active");
+    mainWrapper && mainWrapper.classList.toggle("active");
+
+    if (document.body.clientWidth > 1200) {
+      if (menuToggleButtonIcon.classList.contains("lni-chevron-left")) {
+        menuToggleButtonIcon.classList.remove("lni-chevron-left");
+        menuToggleButtonIcon.classList.add("lni-menu");
+      } else {
+        menuToggleButtonIcon.classList.remove("lni-menu");
+        menuToggleButtonIcon.classList.add("lni-chevron-left");
+      }
+    } else {
+      if (menuToggleButtonIcon.classList.contains("lni-chevron-left")) {
+        menuToggleButtonIcon.classList.remove("lni-chevron-left");
+        menuToggleButtonIcon.classList.add("lni-menu");
+      }
+    }
+  });
+  overlay && overlay.addEventListener("click", () => {
+    sidebarNavWrapper.classList.remove("active");
+    overlay.classList.remove("active");
+    mainWrapper.classList.remove("active");
+  });
+})();
